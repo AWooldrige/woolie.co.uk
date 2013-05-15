96955796650c1d33034ab8a3d90d8b69be4f96db
Alistair Wooldrige <agw@woolie.co.uk>
2013-05-15 22:23:31 +0100

    Adding simple page chrome, less stylesheet, rules

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,9 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+require 'date'
+require 'nanoc/cachebuster'
+include Nanoc::Helpers::CacheBusting
+include Nanoc3::Helpers::LinkTo
+include Nanoc3::Helpers::XMLSitemap
+include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::Blogging
